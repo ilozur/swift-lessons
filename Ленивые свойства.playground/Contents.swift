1c09dbd07fa3a7056da61da3132a9db8257830f8
Edmond Podlegaev <50552703+ilozur@users.noreply.github.com>
2021-12-27 12:34:40 +0300

    Add new files

---
+++ Ленивые свойства.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+
+func bigDataProcessingFunc() -> String {
+  return "very long process"
+}
+
+class Processing {
+  let smallDataProcessing = "small data processing"
+  let averageDataProcessing = "average data processing"
+  lazy var bigDataProcessing = bigDataProcessingFunc()
+}
+
+let process = Processing()
+process.bigDataProcessing
+process
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
