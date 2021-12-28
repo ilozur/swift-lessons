10f4ae8e6b1aba553adf7c4370cd69230086105e
Edmond Podlegaev <50552703+ilozur@users.noreply.github.com>
2021-12-28 11:54:17 +0300

    Add files about access levels

---
+++ Уровни доступа/SimpleProj/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleProj
+//
+//  Created by Ivan Akulov on 21/09/2016.
+//  Copyright © 2016 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+  
+  let someProperty = true
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print(someProperty)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
+
